1543064e3fd3f7ba071aab1bc2c229deeb591641
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-09 15:27:55 +0530

    Add: return sad mood code is added

---
+++ MoodAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string HAPPY = "HAPPY";
+        public string SAD = "SAD";
+        public string AnalyzeMood(string mood)
+        {
+            if (mood.ToLower().Contains("i am in sad mood"))
+            {
+                return SAD;
+            }
+            return null;
+        }
+    }   
+    
+}
